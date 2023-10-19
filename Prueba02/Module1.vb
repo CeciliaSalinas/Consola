2b705d4f94e4ff86945dbc72251f8dd762c5b92b
cecilialujansalinas@outlook.com <cecilialujansalinas@outlook.com>
2023-10-19 13:44:31 -0300

    Agregar archivos de proyecto.

---
+++ Prueba02/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main() 'sub main -> nombreIdentificativo que tiene el procedimiento sub
+
+        'intruccion de código
+        Console.WriteLine("Hola Ceci")
+
+        'instruccion, espera que el usuario ingrese una tecla x teclado y asi el programa se termine y cierre ok.
+        Console.ReadKey(True)
+    End Sub
+
+End Module
